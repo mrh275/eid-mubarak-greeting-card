0807121f7c2903c6d0550f0706f3e0c531e8f03e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-04-21 23:52:55 +0700

    added some icons

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHatWizard, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faBars)
+library.add(faBars, faPlay, faPause)
 
 const app = createApp(App);
 
