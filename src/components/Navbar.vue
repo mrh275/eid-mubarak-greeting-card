81187c12d8892c03bf4e5d03ecf6d39d018a67a7
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-04-21 21:11:25 +0700

    hide navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 
 <template>
-  <nav class="navbar py-2 bg-slate-900 text-white px-4 flex justify-between items-center z-10">
+  <nav class="navbar py-2 hidden bg-slate-900 text-white px-4 justify-between items-center z-10">
     <div class="brand-wrapper">
       <a href="/" class="brand font-semibold text-2xl"><span class="text-[#16db65]">Eid</span> <span class="text-[#7ae582]">Mubarak</span></a>
     </div>
